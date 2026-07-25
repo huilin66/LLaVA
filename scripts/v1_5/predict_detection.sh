717022a03ee0af2bab83428554fbcb10fe156fd7
huilin66 <23039356r@connect.polyu.hk>
2026-07-25 10:26:18 +0800

    Add prediction and model merging scripts for defect detection

---
+++ scripts/v1_5/predict_detection.sh
@@ -0,0 +1,4 @@
+python /localnvme/project/LLaVA-main/scripts/merge_lora_weights.py  \
+       --model-path /localnvme/project/LLaVA-main/checkpoints/llava-v1.5-7b-lora-cubit-det \
+       --model-base /localnvme/project/LLaVA-main/ckpt/llava-v1.5-7b \
+       --save-model-path /localnvme/project/LLaVA-main/checkpoints/llava-v1.5-7b-lora-cubit-det-merge
